e05a4886db14438173cf28ecb4bf3062289cf108
Benjamin Troller <bctroller@gmail.com>
2018-01-03 01:19:23 -0700

    Inital commit with working project

---
+++ argParse.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include "argParse.h"
+
+#define DEFAULT_PADSIZE 65536
+#define NOT_FOUND -1
+
+void pUseExit() {
+   fprintf(stderr, "Usage: otp -e [-i infile] [-o outfile] -p padfile OR\n");
+   fprintf(stderr, "       otp -d [-i infile] [-o outfile] -p padfile OR\n");
+   fprintf(stderr, "       otp -g -p padname [padsize]\n");
+   exit(EXIT_FAILURE);
+}
+
+FILE *openFile(char *fileName, char *mode) {
+   FILE *file;
+
+   if ((file = fopen(fileName, mode)) == NULL) {
+      perror(NULL);
+      exit(EXIT_FAILURE);
+   }
+
+   return file;
+}
+
+int posInArgv(int argc, char *argv[], char *toFind) {
+   int i;
+
+   for (i = 0; i < argc; i++) {
+      if (strcmp(argv[i], toFind) == 0) {
+         return i;
+      }
+   }
+
+   return NOT_FOUND;
+}
+
+FILE *openArgFile(int argc, char *argv[], int pos, char *mode) {
+   FILE *fp;
+
+   if (argc - 1 < pos + 1) {
+      /* If there is no argument after the given flag position */
+      pUseExit();
+   }
+
+   fp = openFile(argv[pos + 1], mode);
+
+   return fp;
+}
+
+void checkPadFileEmpty(ArgInfo *argInfoP) {
+   if (getc(argInfoP -> padFile) == EOF) {
+      fprintf(stderr, "Unable to read from padfile\n");
+      fprintf(stderr, "Make sure you have permission to read the padfile ");
+      fprintf(stderr, "and that it's not empty\n");
+      exit(EXIT_FAILURE);
+   }
+   rewind(argInfoP -> padFile);
+}
+
+void parseEncDec(int argc, char *argv[], ArgInfo *argInfoP) {
+
+   int ipos, opos, ppos, remainingArgs = argc - 2;
+
+   if ((ipos = posInArgv(argc, argv, "-i")) == NOT_FOUND) {
+      argInfoP -> inFile = stdin;
+   }
+   else {
+      argInfoP -> inFile = openArgFile(argc, argv, ipos, "r");
+      remainingArgs -= 2;
+   }
+
+   if ((opos = posInArgv(argc, argv, "-o")) == NOT_FOUND) {
+      argInfoP -> outFile = stdout;
+   }
+   else {
+      argInfoP -> outFile = openArgFile(argc, argv, opos, "w");
+      remainingArgs -= 2;
+   }
+
+   if ((ppos = posInArgv(argc, argv, "-p")) == NOT_FOUND) {
+      pUseExit();
+   }
+   else {
+      argInfoP -> padFile = openArgFile(argc, argv, ppos, "r");
+      remainingArgs -= 2;
+   }
+
+   if (remainingArgs > 0) {
+      pUseExit();
+   }
+
+   checkPadFileEmpty(argInfoP);
+}
+
+void parsePad(int argc, char *argv[], ArgInfo *argInfoP) {
+   int ppos, remainingArgs = argc - 2;
+
+   if ((ppos = posInArgv(argc, argv, "-p")) == NOT_FOUND) {
+      pUseExit();
+   }
+   else {
+      argInfoP -> padFile = openArgFile(argc, argv, ppos, "w");
+      remainingArgs -= 2;
+
+      if (argc - 1 < ppos + 2) {
+         argInfoP -> padSize = DEFAULT_PADSIZE;
+      }
+      else if (strcmp(argv[ppos + 2], "-g") == 0) {
+         argInfoP -> padSize = DEFAULT_PADSIZE;
+      }
+      else {
+         if (sscanf(argv[ppos + 2], "%d", &(argInfoP -> padSize)) == 0) {
+            pUseExit();
+         }
+         else if (argInfoP -> padSize <= 0) {
+            fprintf(stderr, "padsize must be greater than 0 and at most %d\n", 
+               INT_MAX);
+            pUseExit();
+         }
+         remainingArgs -= 1;
+      }
+   }
+
+   if (remainingArgs > 0) {
+      pUseExit();
+   }
+}
+
+void parseArgs(int argc, char *argv[], ArgInfo *argInfoP) {
+
+   /* Check that there is a flag to parse */
+   if (argc < 2) {
+      pUseExit();
+   }
+
+   /* Scan flag's operation-defining character into op */
+   argInfoP -> op = '\0';
+   if (posInArgv(argc, argv, "-e") > NOT_FOUND) {
+      argInfoP -> op = 'e';
+   }
+   else if (posInArgv(argc, argv, "-d") > NOT_FOUND) {
+      argInfoP -> op = 'd';
+   }
+   else if (posInArgv(argc, argv, "-g") > NOT_FOUND) {
+      argInfoP -> op = 'g';
+   }
+
+   /* Parse argv based on op */
+   switch (argInfoP -> op) {
+      case 'e':
+         parseEncDec(argc, argv, argInfoP);
+         break;
+      case 'd':
+         parseEncDec(argc, argv, argInfoP);
+         break;
+      case 'g':
+         parsePad(argc, argv, argInfoP);
+         break;
+      default:
+         pUseExit();
+         break;
+   }
+}
+
+void closeFile(FILE *fp) {
+   if (fclose(fp) == EOF) {
+      perror(NULL);
+      exit(EXIT_FAILURE);
+   }
+}
+
+void closeAllFiles(ArgInfo *argInfoP) {
+   closeFile(argInfoP -> inFile);
+   closeFile(argInfoP -> outFile);
+   closeFile(argInfoP -> padFile);
+}
+
+void closeFiles(ArgInfo *argInfoP) {
+   switch (argInfoP -> op) {
+      case 'e':
+         closeAllFiles(argInfoP);
+         break;
+      case 'd':
+         closeAllFiles(argInfoP);
+         break;
+      case 'g':
+         closeFile(argInfoP -> padFile);
+         break;
+   }
+}
---
+++ crypto.c
@@ -0,0 +1,103 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "crypto.h"
+
+#define RECYCLE 1
+#define NO_RECYCLE 0
+
+unsigned char getShift(ArgInfo *argInfoP, unsigned char *shiftp) {
+   int c;
+
+   if ((c = getc(argInfoP -> padFile)) == EOF) {
+      rewind(argInfoP -> padFile);
+      c = getc(argInfoP -> padFile);
+      *shiftp = (unsigned char)c;
+
+      return RECYCLE;
+   } 
+
+   *shiftp = (unsigned char)c;
+
+   return NO_RECYCLE;
+}
+
+void writeByte(unsigned char inByte, FILE *out) {
+   /* Use buffered output to speed up writing */
+   if (fprintf(out, "%c", inByte) == 0) {
+      perror(NULL);
+      exit(EXIT_FAILURE);
+   }
+}
+
+void encrypt(ArgInfo *argInfoP) {
+   int c;
+   unsigned char inByte, shiftBy, warningShown = 0;
+
+   /* Read in another char from inFile */
+   while ((c = getc(argInfoP -> inFile)) != EOF) {
+      inByte = c;
+      if (getShift(argInfoP, &shiftBy) == RECYCLE && !warningShown) {
+         fprintf(stderr, "Warning: The pad being used is shorter than the"); 
+         fprintf(stderr, " data being encrypted\n");
+         fprintf(stderr, "         To ensure perfect secrecy, use a longer");
+         fprintf(stderr, " pad\n");
+
+         warningShown = 1;
+      }
+      inByte += shiftBy;
+      writeByte(inByte, argInfoP -> outFile);
+   }
+}
+
+void decrypt(ArgInfo *argInfoP) {
+   int c;
+   unsigned char inByte, shiftBy;
+
+   /* Read in another char from inFile */
+   while ((c = getc(argInfoP -> inFile)) != EOF) {
+      inByte = c;
+      getShift(argInfoP, &shiftBy);
+      inByte -= shiftBy;
+      writeByte(inByte, argInfoP -> outFile);
+   }
+}
+
+void seedRand() {
+   time_t seed;
+
+   if ((seed= time(NULL)) == -1) {
+      fprintf(stderr, "Unable to retrieve system time\n");
+      exit(EXIT_FAILURE);
+   }
+   srand(seed);
+}
+
+void generatePad(ArgInfo *argInfoP) {
+   int byteCount;
+
+   seedRand();
+
+   for (byteCount = 0; byteCount < argInfoP -> padSize; byteCount++) {
+      writeByte(rand(), argInfoP -> padFile);
+   }
+}
+
+void evalArgs(ArgInfo *argInfoP) {
+   /* Note: It's not possible for op to be anything other than e, d, or g */
+   switch (argInfoP -> op) {
+      case 'e':
+         encrypt(argInfoP);
+         break;
+      case 'd':
+         decrypt(argInfoP);
+         break;
+      case 'g':
+         generatePad(argInfoP);
+         break;
+      default:
+         fprintf(stderr, "op not recognized in file %s at line %d\n", __FILE__,
+            __LINE__);
+         exit(EXIT_FAILURE);
+   }
+}
