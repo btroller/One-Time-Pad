3a982722f64e6fe02194b4c0768d214eb11bf249
Benjamin Troller <bctroller@gmail.com>
2018-02-02 22:23:34 -0800

    Temporarily simplified tests

--- test.sh
+++ test.sh
@@ -1,56 +1,13 @@
 #!/bin/bash
 
-#Search for .in files in the current directory
-fileFound=false
-for file in ./*.in
-do
-   fileFound=true;
-   break
-done
+./otp -g -p pad
+./otp -e -o enc -p pad < test05.in
+./otp -d -i enc -o dec -p pad
 
-#Check if .in files exist in the current directory
-#   If not, complain to user and exit
-if [ $fileFound = false ]; then
-   echo "Error: no files with .in extension present"
+diff -q test05.in dec
+
+if [ $? != 0 ]; then
    exit 1
 fi
 
-make
-
-#Check if otp file exists in the current directory
-#   If not, complain to user and exit
-if [ ! -f ./otp ]; then
-   echo "Error: no otp executable present"
-   exit 1
-fi
-
-echo
-echo "Performing tests: "
-for testInput in *.in; do
-
-   # Run test 1
-   ./otp -g -p EX_PAD
-   ./otp -e -i $testInput -o $testInput.enc -p EX_PAD
-   ./otp -d -i $testInput.enc -o $testInput.dec -p EX_PAD
-
-   # diff the results
-   diff -q $testInput $testInput.dec
-
-   # remove temp files
-   rm $testInput.enc $testInput.dec EX_PAD
- 
-
-   # Run test 2
-   ./otp -p EX_PAD 7000000 -g
-   ./otp -i $testInput -p EX_PAD -o $testInput.enc -e
-   ./otp -p EX_PAD -i $testInput.enc -d -o $testInput.dec 
-
-   # diff the results
-   diff -q $testInput $testInput.dec
-
-   # remove temp files
-   rm $testInput.enc $testInput.dec EX_PAD
-
-done
-
-exit
+rm pad enc dec
